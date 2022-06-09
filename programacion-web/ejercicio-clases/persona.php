70c197abb3d026d81af6a78765ed61a3aa29a22a
Sebastián Atienza <102329383+SebAti95@users.noreply.github.com>
2022-06-09 13:49:23 -0300

    Add files via upload

---
+++ programacion-web/ejercicio-clases/persona.php
@@ -0,0 +1,45 @@
+<?php
+    class Persona {
+        private $cedula;
+        private $nombre1;
+        private $nombre2;
+        private $apellido1;
+        private $apellido2;
+
+        public Persona{
+        }
+
+        public function getCedula() {
+            return $this->cedula;
+        }
+        public function getNombre1() {
+            return $this->nombre1;
+        }
+        public function getNombre2() {
+            return $this->nombre2;
+        }
+        public function getApellido1() {
+            return $this->apellido1;
+        }
+        public function getApellido2() {
+            return $this->apellido2;
+        }
+
+        public function setCedula($cedula) {   
+            $this->cedula = $cedula;
+        }
+        public function setNombre1($nombre1) {   
+            $this->nombre1 = $nombre1;
+        }
+        public function setNombre2($nombre2) {   
+            $this->nombre2 = $nombre2;
+        }
+        public function setApellido1($apellido1) {   
+            $this->apellido1 = $apellido1;
+        }
+        public function setApellido2($apellido2) {   
+            $this->apellido2 = $apellido2;
+        }
+    }
+
+?>
